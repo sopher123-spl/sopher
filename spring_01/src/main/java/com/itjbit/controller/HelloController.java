a07fe388ad43d9e51e15e3c8681f2b4327b536c4
sopher <756752122@qq.com>
2020-02-15 17:02:14 +0800

    进行了简单的调整

--- spring_01/src/main/java/com/itjbit/controller/HelloController.java
+++ spring_01/src/main/java/com/itjbit/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
     @RequestMapping("/hehe")
     public @ResponseBody Address hello(@RequestBody Address address){
         System.out.println(address.getAddressName());
-        address.setAddressName("haha");
+        //address.setAddressName("haha");
         return address;
     }
 
